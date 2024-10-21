ebcceb0d6cf0c6de231a6f5d2a234e1c89792eca
uf4te <66998280+uf4te@users.noreply.github.com>
2024-10-21 22:15:58 +0800

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -10,18 +10,16 @@ function generateMD5(email) {
     });
 }
 
-
 module.exports = Application({
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
   },
 
-
   /*
-此方法或传入一个 comment 对象，你可以通过 comment.mail 获取邮箱。若返回值为 string 类型，则会直接调用返回值作为头像地址，否则正常生成 MD5。
-*/
-async avatarUrl(comment) {
+  此方法会传入一个 comment 对象，你可以通过 comment.mail 获取邮箱。若返回值为 string 类型，则会直接调用返回值作为头像地址，否则正常生成 MD5。
+  */
+  async avatarUrl(comment) {
     const mail = comment.mail;
 
     // 检查是否填写邮箱，若未填写，则不处理头像
@@ -30,7 +28,6 @@ async avatarUrl(comment) {
     }
     
     const reg = new RegExp('(\\d+)@qq\\.com$', 'i');
-    const mail = comment.mail;
 
     // 匹配 QQ 邮箱
     if (reg.test(mail)) {
@@ -39,11 +36,11 @@ async avatarUrl(comment) {
     }
 
     // 匹配非 QQ 邮箱
-    if (!reg.test(mail)) {
-        // 生成邮箱的 MD5 哈希值
-        const md5Hash = await generateMD5(mail.trim().toLowerCase());
-        // 返回 Gravatar 头像链接
-        return 'https://gravatar.loli.net/avatar/' + md5Hash;
+    else {
+      // 生成邮箱的 MD5 哈希值
+      const md5Hash = await generateMD5(mail.trim().toLowerCase());
+      // 返回 Gravatar 头像链接
+      return 'https://gravatar.loli.net/avatar/' + md5Hash;
     }
   },
 
